caa4c433c519b6effab1679c77441960394eea63
Nics <NicsTr@users.noreply.github.com>
2023-11-11 20:08:01 +0100

    Init

---
+++ dot_chezmoi_dependencies.sh
@@ -0,0 +1,19 @@
+apt install sway # sway
+apt install swaylock # swaylock
+apt install waybar # waybar
+apt install wofi # wofi
+apt install wlogout # wlogout
+snap install alacritty # alacritty
+snap install helix --classic # helix
+apt install light
+apt install arandr
+apt install playerctl
+apt install blueman
+apt install sway-notification-center
+apt install zsh # zsh
+# Meslo Nerd Font (https://github.com/romkatv/powerlevel10k/blob/master/font.md)
+# powerlevel10k (https://github.com/romkatv/powerlevel10k#installation)
+
+apt install pinta # for screenshot editing
+# apt install flameshot xdg-desktop-portal xdg-desktop-portal-wlr grim
+# https://github.com/flameshot-org/flameshot/blob/master/docs/Sway%20and%20wlroots%20support.md
